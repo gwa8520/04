1eaf4a449d5f65e1fb68474c0aef90fe51c4723c
gwa8520 <144990431+gwa8520@users.noreply.github.com>
2023-10-05 14:55:21 +0900

    23.10.05 2

---
+++ 프로그래밍 스터디 나누기나머지 연산자.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int main(void) {
+	
+	int second;
+	
+	printf("enter the sec");
+	scanf("%d",&second);
+	
+	printf("%d Minutes and %d seconds.",second/60,second%60);
+	
+	return 0;
+	
+}
